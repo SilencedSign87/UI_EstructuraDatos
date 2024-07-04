27f3f63688d96cf021b786001576b14acc8d3c0f
SilencedSign87 <diazvegabruno@hotmail.com>
2024-07-03 23:10:25 -0500

    añadido: labels y textbox para registro de usuarios

--- frm_NuevoVendedor.h
+++ frm_NuevoVendedor.h
@@ -22,6 +22,13 @@ namespace GUIEstructuraDeDatos {
 
 			this->prevForm = prevForm;
 		}
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	public:
 	private: System::Windows::Forms::Form^ prevForm;
 
 	protected:
@@ -75,9 +82,16 @@ namespace GUIEstructuraDeDatos {
 			this->tab_listaV = (gcnew System::Windows::Forms::TabPage());
 			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->Tabla_vendedores = (gcnew System::Windows::Forms::DataGridView());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->label4 = (gcnew System::Windows::Forms::Label());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->panel1->SuspendLayout();
 			this->tabControl1->SuspendLayout();
+			this->tab_nuevoV->SuspendLayout();
 			this->tab_listaV->SuspendLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Tabla_vendedores))->BeginInit();
 			this->SuspendLayout();
@@ -101,7 +115,7 @@ namespace GUIEstructuraDeDatos {
 			this->label6->ForeColor = System::Drawing::Color::White;
 			this->label6->Location = System::Drawing::Point(20, 192);
 			this->label6->Name = L"label6";
-			this->label6->Size = System::Drawing::Size(210, 56);
+			this->label6->Size = System::Drawing::Size(192, 50);
 			this->label6->TabIndex = 3;
 			this->label6->Text = L"Sistema de Almacén\r\ny Ventas";
 			this->label6->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
@@ -165,10 +179,16 @@ namespace GUIEstructuraDeDatos {
 			// 
 			// tab_nuevoV
 			// 
-			this->tab_nuevoV->Location = System::Drawing::Point(4, 34);
+			this->tab_nuevoV->Controls->Add(this->textBox3);
+			this->tab_nuevoV->Controls->Add(this->label4);
+			this->tab_nuevoV->Controls->Add(this->textBox2);
+			this->tab_nuevoV->Controls->Add(this->label3);
+			this->tab_nuevoV->Controls->Add(this->textBox1);
+			this->tab_nuevoV->Controls->Add(this->label2);
+			this->tab_nuevoV->Location = System::Drawing::Point(4, 33);
 			this->tab_nuevoV->Name = L"tab_nuevoV";
 			this->tab_nuevoV->Padding = System::Windows::Forms::Padding(3);
-			this->tab_nuevoV->Size = System::Drawing::Size(714, 619);
+			this->tab_nuevoV->Size = System::Drawing::Size(714, 620);
 			this->tab_nuevoV->TabIndex = 0;
 			this->tab_nuevoV->Text = L"Registrar nuevo vendedor";
 			this->tab_nuevoV->UseVisualStyleBackColor = true;
@@ -177,10 +197,10 @@ namespace GUIEstructuraDeDatos {
 			// 
 			this->tab_listaV->Controls->Add(this->label1);
 			this->tab_listaV->Controls->Add(this->Tabla_vendedores);
-			this->tab_listaV->Location = System::Drawing::Point(4, 34);
+			this->tab_listaV->Location = System::Drawing::Point(4, 33);
 			this->tab_listaV->Name = L"tab_listaV";
 			this->tab_listaV->Padding = System::Windows::Forms::Padding(3);
-			this->tab_listaV->Size = System::Drawing::Size(714, 619);
+			this->tab_listaV->Size = System::Drawing::Size(714, 620);
 			this->tab_listaV->TabIndex = 1;
 			this->tab_listaV->Text = L"Lista de vendedores";
 			this->tab_listaV->UseVisualStyleBackColor = true;
@@ -191,7 +211,7 @@ namespace GUIEstructuraDeDatos {
 			this->label1->ForeColor = System::Drawing::SystemColors::Highlight;
 			this->label1->Location = System::Drawing::Point(16, 24);
 			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(492, 26);
+			this->label1->Size = System::Drawing::Size(442, 24);
 			this->label1->TabIndex = 1;
 			this->label1->Text = L"A continuación se muestra la lista de vendedores";
 			// 
@@ -205,21 +225,72 @@ namespace GUIEstructuraDeDatos {
 			this->Tabla_vendedores->Size = System::Drawing::Size(701, 493);
 			this->Tabla_vendedores->TabIndex = 0;
 			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(17, 17);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(226, 24);
+			this->label2->TabIndex = 0;
+			this->label2->Text = L"Nuevo nombre de usuario:";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(21, 45);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(280, 27);
+			this->textBox1->TabIndex = 1;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(21, 112);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(280, 27);
+			this->textBox2->TabIndex = 3;
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(17, 84);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(163, 24);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Nueva contraseña:";
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(21, 182);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(280, 27);
+			this->textBox3->TabIndex = 5;
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(17, 154);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(235, 24);
+			this->label4->TabIndex = 4;
+			this->label4->Text = L"Nueva nombre de vendedor:";
+			// 
 			// frm_NuevoVendedor
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(984, 659);
+			this->ClientSize = System::Drawing::Size(984, 661);
 			this->Controls->Add(this->tabControl1);
 			this->Controls->Add(this->panel1);
 			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->MinimumSize = System::Drawing::Size(1000, 700);
 			this->Name = L"frm_NuevoVendedor";
 			this->Text = L"Gestion de Vendedores";
+			this->FormClosing += gcnew System::Windows::Forms::FormClosingEventHandler(this, &frm_NuevoVendedor::frm_NuevoVendedor_FormClosing);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->panel1->ResumeLayout(false);
 			this->panel1->PerformLayout();
 			this->tabControl1->ResumeLayout(false);
+			this->tab_nuevoV->ResumeLayout(false);
+			this->tab_nuevoV->PerformLayout();
 			this->tab_listaV->ResumeLayout(false);
 			this->tab_listaV->PerformLayout();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->Tabla_vendedores))->EndInit();
@@ -233,5 +304,10 @@ namespace GUIEstructuraDeDatos {
 			this->Close();
 		}
 	}
-};
+	private: System::Void frm_NuevoVendedor_FormClosing(System::Object^ sender, System::Windows::Forms::FormClosingEventArgs^ e) {
+		if (prevForm != nullptr) {
+			prevForm->Show();
+		}
+	}
+	};
 }
